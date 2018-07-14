cd53e3124fe042e66928f7f3b301cb4d283e40b6
unknown <johntae.stroud@gmail.com>
2018-07-14 01:06:02 -0400

    Implemented homepage background, links, and styles

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
-
+import { Grid, Cell } from 'react-mdl';
 
 class Home extends Component {
     render() {
         return (
-            <div><h1>Home</h1></div>
+            <div style={{ width: '100%', margin: 'auto'}}>
+                <Grid className="home-grid">
+                  <Cell col={12}>
+                    <img src="" alt="" className="profile-img"/>
+                    <div className="title-text">
+                        <h1>Software Developer</h1>
+                        <hr/>
+
+                        <p>HTML/CSS | JavaScript | React | PHP </p>
+                        
+                        <div className="social-links">
+                             {/* Github */}
+                             <a href="https://github.com/johntaestroud" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-github-square" aria-hidden="true" />
+                            </a>
+                            {/* Contact */}
+                            <a href="mailto:johntae.stroud@gmail.com" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-envelope-o" aria-hidden="true" />
+                            </a>
+                            {/* Instagram */}
+                            <a href="https://www.instagram.com/talentfamer/" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-instagram" aria-hidden="true" />
+                            </a>
+                        </div>
+                    </div>
+                  </Cell>
+                </Grid>
+            </div>
         )
     }
 }
